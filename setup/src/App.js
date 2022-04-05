7ab51d5e39a2cb83fabf2e371257c7f243a2d4ec
Muhammad Asif <asif88383@gmail.com>
2022-04-05 16:48:53 +0500

    first commit

--- setup/src/App.js
+++ setup/src/App.js
@@ -4,10 +4,17 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
+
+  const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([allCategories]);
 
   const filterItems = (category) => {
+    if(category === 'all') {
+      setMenuItems(items);
+      return;
+    }
     const newItems = items.filter(item => item.category === category);
     setMenuItems(newItems);
   }
@@ -19,7 +26,7 @@ function App() {
           <h2>Our Menu</h2>
           <div className='underline'></div>
         </div>
-        <Categories filterItems={filterItems}/>
+        <Categories categories={categories} filterItems={filterItems}/>
         <Menu items={menuItems} />
       </section>
     </main>
